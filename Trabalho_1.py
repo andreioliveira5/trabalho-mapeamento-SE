829188b33fe52b4efdbf6747b7b6d6572e92ef8a
lrigo429 <70539918+lrigo429@users.noreply.github.com>
2020-09-08 20:43:01 -0300

    Inserção dos dados na matriz

--- Trabalho_1.py
+++ Trabalho_1.py
@@ -22,6 +22,17 @@ for ind_tests in range(numero_teste):
     apps = mpsoc['apps']
     numero_apps = (len(apps))
 
+    numero_clusters = int((mpsoc_x/cluster_x)*(mpsoc_y/cluster_y))
+    cluster = 0
+
+    matriz_tri = np.zeros([tasks_per_pe, mpsoc_x, mpsoc_y]) # cria a matrix ([profuncidade, linhas, colunas])
+    linha = 0
+    coluna = 0
+    profundidade = 0
+
+    soma_linha = 0
+    soma_coluna = 0
+
 
     print('\n')
     print('Teste ', mpsoc['id'])
@@ -31,26 +42,55 @@ for ind_tests in range(numero_teste):
         for c in range(len(dados_apps)):#for para buscar as especificações de cada app
             app_test = dados_apps[c]
             name = app_test['name']
+            qtd_apps = app['qtd_apps']
             if app_name == name:
                 print(name)
                 tasks = app_test['tasks']
-                for d in range(app_test['number_tasks']):#for para buscar as tarefas de cada app
-                    task = tasks[d]
-                    ID = task['id']
-                    print(ID)
+                for e in range(1):#for para rodas tantas vezes um app ####qtd_apps######?????????
+                    for d in range(app_test['number_tasks']):#for para buscar as tarefas de cada app
+                        task = tasks[d]
+                        ID = task['id']
+                        print(ID)
+                        matriz_tri[profundidade,(linha+soma_linha),(coluna+soma_coluna)] = ID #adiciona os dados na matriz 
+                        if profundidade < (tasks_per_pe-1):#vai mudando as possições para salvar as tarefas
+                            profundidade +=1
+                        else:
+                            profundidade = 0
+                            if (coluna+soma_coluna) < (mpsoc_y-1):
+                                coluna += 1 
+                            else:
+                                coluna = 0
+                                if (linha+soma_linha) < (mpsoc_x-1):
+                                    linha += 1
+                                else:
+                                    linha = 0
+
+                if cluster < (numero_clusters-1):#muda de cluster para cada app
+                        cluster +=1
+                        [soma_linha,soma_coluna]=f.posicao_cluster(cluster, mpsoc_x, mpsoc_y, cluster_x, cluster_y)
+                        limha = 0
+                        coluna = 0
+                        profundidade = 0
+                else:
+                    cluster = 0
+
+            
 
 
 
 
 
-   # matrix_tri = np.zeros([tasks_per_pe, mpsoc_x, mpsoc_y]) # cria a matrix ([profuncidade, linhas, colunas])
-   # f.mostrar(matrix_tri, mpsoc_x, mpsoc_y, cluster_x, cluster_y, tasks_per_pe)
+   
+    f.mostrar(matriz_tri, mpsoc_x, mpsoc_y, cluster_x, cluster_y, tasks_per_pe)
    
     
     
 
            
-#[i,j] = f.localiza(matrix_tri, 1, 2, mpsoc_x, mpsoc_y, cluster_x,cluster_y)
+#[i,j] = f.localiza(matriz_tri, 1, 2, mpsoc_x, mpsoc_y, cluster_x,cluster_y)
 #print(i)
 #print(j)
 
+def inserir():
+
+    return()
